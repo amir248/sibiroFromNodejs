734a327b50ab0d11c259d02d14e7b667d380b27b
chikchicly <chikchicly@gmail.com>
2023-08-20 05:47:09 +0700

    oK

--- public/script/javascript.js
+++ public/script/javascript.js
@@ -1,13 +1,64 @@
-let menu=document.createElement('p');
-menu.setAttribute('id','menu');
-document.querySelector('nav').prepend(menu);
+if(innerWidth<700){
+  let menu=document.createElement('p');
+  menu.setAttribute('id','menu');
+  menu.style.cssText=`z-index:9;`;
+  document.querySelector('nav').prepend(menu);
 
-let spanOne=document.createElement('span');
-spanOne.style.cssText=`width:37px;height:3px;background:black;display:block;`;
-document.querySelector('#menu').prepend(spanOne);
-let spanTwo=document.createElement('span');
-spanTwo.style.cssText=`width:37px;height:3px;background:black;display:block;margin-top:17%;margin-bottom:17%;`;
-document.querySelector('#menu').prepend(spanTwo);
-let spanThree=document.createElement('span');
-spanThree.style.cssText=`width:37px;height:3px;background:black;display:block`;
-document.querySelector('#menu').prepend(spanThree);
+  let spanOne=document.createElement('span');
+  spanOne.style.cssText=`width:37px;height:3px;background:black;display:block;`;
+  document.querySelector('#menu').prepend(spanOne);
+  let spanTwo=document.createElement('span');
+  spanTwo.style.cssText=`width:37px;height:3px;background:black;display:block;margin-top:17%;margin-bottom:17%;`;
+  document.querySelector('#menu').prepend(spanTwo);
+  let spanThree=document.createElement('span');
+  spanThree.style.cssText=`width:37px;height:3px;background:black;display:block`;
+  document.querySelector('#menu').prepend(spanThree);
+}else{
+  console.log('thre')
+}
+let countClick=+0;
+document.querySelector('#menu').addEventListener('click',()=>{
+  countClick++;
+  if(countClick==1){
+    let newBox=document.createElement('div');
+    newBox.style.cssText=`
+      display:flex;
+      position:fixed;
+      justify-content:center;
+      align-items:center;
+      width:100%;
+      height:100vh;
+      background:black;
+      top:0;
+      z-index:0;
+      background:white;
+      flex-direction:column;
+    `;
+    newBox.innerHTML=`
+      <h1 id='ok'>oK</h1>
+      <h1 id='home'>home</h1>
+      <h1 id='footer'>footer</h1>
+    `;
+    document.querySelector('body').append(newBox);
+    document.querySelector('body > div').addEventListener('click',()=>{
+      document.querySelector('body > div').remove();
+      countClick=0;
+    });
+    document.querySelector('#ok').addEventListener('click',()=>{
+      console.log('ok');
+      window.location.href='#ok';
+    });
+    document.querySelector('#footer').addEventListener('click',()=>{
+      console.log('footer');
+      window.location.href=' ';
+    });
+    document.querySelector('#home').addEventListener('click',()=>{
+      console.log('home');
+      window.location.href'#oo';
+    });
+  }else if(countClick==2){
+    document.querySelector('body > div').remove();
+    countClick=0;
+  }
+  console.log('click');
+});
