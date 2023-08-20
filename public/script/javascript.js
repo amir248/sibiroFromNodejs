fde22b5e3422d85caf192fa1fe2d2fb3fb9d6389
chikchicly <chikchicly@gmail.com>
2023-08-20 17:38:10 +0700

    oK

--- public/script/javascript.js
+++ public/script/javascript.js
@@ -140,7 +140,7 @@ setInterval(displayWidth,7000);
 let clickFlipping=document.createElement('span');
 clickFlipping.innerHTML=`Фотография листатается нажатием`;
 document.querySelector('#photos').append(clickFlipping);
-const arrayPhoto=['https://spb.stilkuhni.ru/upload/iblock/cea/029b18009d4fc63dcbce5c217f91c77a.png','https://ritzmebel.ru/wp-content/themes/provodnik/assets/images/production_1.jpg','https://geometria-loft.ru/wp-content/uploads/2018/10/1-2.jpg']
+const arrayPhoto=['https://n1s2.hsmedia.ru/66/99/3d/66993d265ae81bfb2eac171053f2c010/1597x892_0xac120003_1644557031599829157.jpg','https://spb.stilkuhni.ru/upload/iblock/cea/029b18009d4fc63dcbce5c217f91c77a.png','https://ritzmebel.ru/wp-content/themes/provodnik/assets/images/production_1.jpg','https://geometria-loft.ru/wp-content/uploads/2018/10/1-2.jpg']
 let clickPhoto=+0;
 document.querySelector('body > main:nth-child(2) > div:nth-child(2) > div:nth-child(1) > img:nth-child(1)').addEventListener('click',()=>{
   clickPhoto++;
