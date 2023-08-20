04cc0dcd3b75e3e5588a86d450549731a30a6358
chikchicly <chikchicly@gmail.com>
2023-08-20 15:46:31 +0700

    oK

--- public/script/javascript.js
+++ public/script/javascript.js
@@ -1,74 +1,136 @@
-  if(innerWidth<700){
-    let menu=document.createElement('p');
-    menu.setAttribute('id','menu');
-    menu.style.cssText=`z-index:9;position:fixed;top:17px;`;
-    document.querySelector('nav').prepend(menu);
+function closeMenuSpan(){
+  document.querySelector('#menu > span:nth-child(2)').style.cssText=`
+  transition: all 1s ease-out;width:37px;height:3px;background:black;display:block;margin-top:17%;margin-bottom:17%;margin-right:17px;
+  `;
+  document.querySelector('#menu > span:nth-child(1)').style.cssText=`
+  transition: all 1s ease-out;width:37px;height:3px;background:black;display:block;
+  `;
+  document.querySelector('#menu > span:nth-child(3)').style.cssText=`
+  transition: all 1s ease-out;width:37px;height:3px;background:black;display:block
+  `;
+  document.querySelector('body > div').style.cssText=`
+  width:1px;
+  transition: all 1s ease-out;
+  opacity:0;
+  `;
+}
+if(innerWidth<700){
+  let menu=document.createElement('p');
+  menu.setAttribute('id','menu');
+  menu.style.cssText=`z-index:9;position:fixed;top:17px;`;
+  document.querySelector('nav').prepend(menu);
 
-    let spanOne=document.createElement('span');
-    spanOne.style.cssText=`width:37px;height:3px;background:black;display:block;`;
-    document.querySelector('#menu').prepend(spanOne);
-    let spanTwo=document.createElement('span');
-    spanTwo.style.cssText=`width:37px;height:3px;background:black;display:block;margin-top:17%;margin-bottom:17%;`;
-    document.querySelector('#menu').prepend(spanTwo);
-    let spanThree=document.createElement('span');
-    spanThree.style.cssText=`width:37px;height:3px;background:black;display:block`;
-    document.querySelector('#menu').prepend(spanThree);
-  }else{
-    console.log('thre')
+  let spanOne=document.createElement('span');
+  spanOne.style.cssText=`transition: all 1s ease-out;width:37px;height:3px;background:black;display:block;`;
+  document.querySelector('#menu').prepend(spanOne);
+  let spanTwo=document.createElement('span');
+  spanTwo.style.cssText=`transition: all 1s ease-out;width:37px;height:3px;background:black;display:block;margin-top:17%;margin-bottom:17%;margin-right:17px;`;
+  document.querySelector('#menu').prepend(spanTwo);
+  let spanThree=document.createElement('span');
+  spanThree.style.cssText=`transition: all 1s ease-out;width:37px;height:3px;background:black;display:block`;
+  document.querySelector('#menu').prepend(spanThree);
+}else{
+  console.log('thre');
+  if(document.querySelector('body > div')==true){
+    console.log('body > div TRUE');
   }
-  let countClick=+0;
-  document.querySelector('#menu').addEventListener('click',()=>{
-    countClick++;
-    if(countClick==1){
-      let newBox=document.createElement('div');
+}
+let countClick=+0;
+document.querySelector('#menu').addEventListener('click',()=>{
+  countClick++;
+  if(countClick==1){
+    document.querySelector('#menu > span:nth-child(2)').style.cssText=`
+    margin-bottom:0;
+    margin-top:0;
+    display:none;
+    margin:0;
+    transition: all 1s ease-out;
+    opacity:0;
+    margin-right:17px;
+    `;
+    document.querySelector('#menu > span:nth-child(1)').style.cssText=`
+      transition: all 1s ease-out;
+      width:45px;
+      height:3px;
+      background:black;
+      display:block;
+      transition: all 1s ease-out;
+      transform: rotate(50deg);
+      margin-right:17px;
+      margin-top:17px;
+
+    `;
+    document.querySelector('#menu > span:nth-child(3)').style.cssText=`
+    transition: all 1s ease-out;
+    width:45px;
+    height:3px;
+    background:black;
+    display:block;
+    transform: rotate(-50deg);
+    margin-right:17px;
+    `;
+    let newBox=document.createElement('div');
+    newBox.className='openMenu';
+    newBox.innerHTML=`
+    <h1 id="ok-menu">ok</h1>
+    <h1 id="photo-menu">photo</h1>
+    <h1 id="home-menu">home</h1>
+    <h1 id="footer-menu">footer</h1>
+    `;
+    setInterval(()=>{
       newBox.style.cssText=`
+      opacity:1;
+      width:100%;
+      transition: all 1s ease-out;
       display:flex;
       position:fixed;
       justify-content:center;
       align-items:center;
-      width:100%;
       height:100vh;
-      background:black;
+      // background:black;
       top:0;
       z-index:0;
       background:white;
       flex-direction:column;
-      `;
-      newBox.innerHTML=`
-      <h1 id="ok-menu">ok</h1>
-      <h1 id="photo-menu">photo</h1>
-      <h1 id="home-menu">home</h1>
-      <h1 id="footer-menu">footer</h1>
-      `;
-      document.querySelector('body').append(newBox);
-      document.querySelector('body > div').addEventListener('click',()=>{
-        document.querySelector('body > div').remove();
-        countClick=0;
-      });
-      document.querySelector('#ok-menu').addEventListener('click',()=>{
-        console.log('ok');
-        window.location.href='#oks';
-      });
-      document.querySelector('#footer-menu').addEventListener('click',()=>{
-        console.log('footer');
-        window.location.href='#footer';
-      });
-      document.querySelector('#home-menu').addEventListener('click',()=>{
-        console.log('home');
-        window.location.href='#';
-      });
-      document.querySelector('#photo-menu').addEventListener('click',()=>{
-        console.log('photo');
-        window.location.href='#photos';
-      });
-    }else if(countClick==2){
+      `
+    },300);
+    document.querySelector('body').append(newBox);
+    document.querySelector('body > div').addEventListener('click',()=>{
+      closeMenuSpan();
       document.querySelector('body > div').remove();
       countClick=0;
-    }
-    console.log('click');
-  });
+    });
+    document.querySelector('#ok-menu').addEventListener('click',()=>{
+      console.log('ok');
+      window.location.href='#oks';
+      closeMenuSpan();
+    });
+    document.querySelector('#footer-menu').addEventListener('click',()=>{
+      console.log('footer');
+      window.location.href='#footer';
+      closeMenuSpan();
+    });
+    document.querySelector('#home-menu').addEventListener('click',()=>{
+      console.log('home');
+      window.location.href='#';
+      closeMenuSpan();
+    });
+    document.querySelector('#photo-menu').addEventListener('click',()=>{
+      console.log('photo');
+      window.location.href='#photos';
+      closeMenuSpan();
+    });
+  }else if(countClick==2){
+    closeMenuSpan();
+    setTimeout(()=>{
+      document.querySelector('body > div').remove();
+      countClick=0;
+    },70);
+  }
+  console.log('click');
+});
 function displayWidth(){
     console.log('oooK');
 }
 window.addEventListener('DOMContentLoaded',displayWidth);
-setInterval(displayWidth,700);
+setInterval(displayWidth,7000);
