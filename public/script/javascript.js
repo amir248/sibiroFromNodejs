e32a7f072f88a8fffed14667a8450eeace08cac4
chikchicly <chikchicly@gmail.com>
2023-08-28 16:56:33 +0700

    oK

--- public/script/javascript.js
+++ public/script/javascript.js
@@ -446,9 +446,12 @@ if(window.innerWidth>700){
 // setInterval(displayWidth,7000);
 
 //inscription more photos click flipping
-let clickFlipping=document.createElement('span');
-clickFlipping.innerHTML=`<span style='width:100%;max-width:700px;overflow:hidden;'>Фотографии листатается нажатием</span>`;
-document.querySelector('#photos').append(clickFlipping);
+if(window.innerWidth<700){
+  let clickFlipping=document.createElement('span');
+  clickFlipping.innerHTML=`<span style='width:100%;max-width:700px;overflow:hidden;'>Фотографии листатается нажатием</span>`;
+  document.querySelector('#photos').append(clickFlipping);
+}
+
 const arrayPhoto=['https://n1s2.hsmedia.ru/66/99/3d/66993d265ae81bfb2eac171053f2c010/1597x892_0xac120003_1644557031599829157.jpg','https://spb.stilkuhni.ru/upload/iblock/cea/029b18009d4fc63dcbce5c217f91c77a.png','https://ritzmebel.ru/wp-content/themes/provodnik/assets/images/production_1.jpg','https://geometria-loft.ru/wp-content/uploads/2018/10/1-2.jpg']
 let clickPhoto=+0;
 document.querySelector('#photos > .imge > img').addEventListener('click',()=>{
